093948aa44fb6e9d7dd7603d68e504a40b40a330
AngelArredondo20 <angelarredondoavalos20@gmail.com>
2025-09-15 20:19:01 -0600

    Implementation NotificationsUser accessor

--- Accessors/NotificationsUser.cs
+++ Accessors/NotificationsUser.cs
@@ -1,121 +1,121 @@
-﻿//using ANIMALITOS_PHARMA_API.Contracts;
-//using Microsoft.EntityFrameworkCore;
+﻿using ANIMALITOS_PHARMA_API.Contracts;
+using Microsoft.EntityFrameworkCore;
 
-//namespace ANIMALITOS_PHARMA_API.Accessors
-//{
-//    public partial class AnimalitosClient
-//    {
-//        public IEnumerable<NotificationsUser> GetListNotificationsUser(NotificationsUserFilter filter)
-//        {
-//            IQueryable<Models.NotificationsUser> query = from m in _EntityContext.NotificationsUsers select m;
-//            query = query.AsNoTracking();
+namespace ANIMALITOS_PHARMA_API.Accessors
+{
+    public partial class AnimalitosClient
+    {
+        public IEnumerable<NotificationsUser> GetListNotificationsUser(NotificationsUserFilter filter)
+        {
+            IQueryable<Models.NotificationsUser> query = from m in _EntityContext.NotificationsUsers select m;
+            query = query.AsNoTracking();
 
-//            if (filter.Id > 0)
-//                query = query.Where(m => m.Id == filter.Id);
-//            if (filter.IsRead != null)
-//                query = query.Where(m => m.IsRead == filter.IsRead);
-//            if (filter.UserId > 0)
-//                query = query.Where(m => m.UserId == filter.UserId);
-//            if (filter.StatusId != 0)
-//                query = query.Where(m => m.StatusId == filter.StatusId);
+            if (filter.Id > 0)
+                query = query.Where(m => m.Id == filter.Id);
+            if (filter.IsRead != null)
+                query = query.Where(m => m.IsRead == filter.IsRead);
+            if (filter.UserId > 0)
+                query = query.Where(m => m.UserId == filter.UserId);
+            if (filter.StatusId != 0)
+                query = query.Where(m => m.StatusId == filter.StatusId);
 
-//            if (!string.IsNullOrWhiteSpace(filter.SortColumn))
-//                query = query.OrderBy(query => filter.SortColumn);
+            if (!string.IsNullOrWhiteSpace(filter.SortColumn))
+                query = query.OrderBy(query => filter.SortColumn);
 
-//            if (filter.PagingBegin > -1)
-//                query = query.Skip(filter.PagingBegin);
-//            if (filter.PagingRange > -1)
-//                query = query.Take(filter.PagingRange);
+            if (filter.PagingBegin > -1)
+                query = query.Skip(filter.PagingBegin);
+            if (filter.PagingRange > -1)
+                query = query.Take(filter.PagingRange);
 
-//            var tempStuff = query.ToList();
-//            List<NotificationsUser> context = new List<NotificationsUser>();
-//            foreach (Models.NotificationsUser tempitem in tempStuff)
-//                context.Add(ConvertNotificationsUser_ToAccessorContract(tempitem));
+            var tempStuff = query.ToList();
+            List<NotificationsUser> context = new List<NotificationsUser>();
+            foreach (Models.NotificationsUser tempitem in tempStuff)
+                context.Add(ConvertNotificationsUser_ToAccessorContract(tempitem));
 
-//            return context;
-//        }
+            return context;
+        }
 
-//        public NotificationsUser GetNotification(int id)
-//        {
-//            if (id <= 0)
-//                throw new Exception("Cannot search object without an Id.");
+        public NotificationsUser GetNotificationUser(int id)
+        {
+            if (id <= 0)
+                throw new Exception("Cannot search object without an Id.");
 
-//            var objTemp = _EntityContext.Notifications.SingleOrDefault(m => m.Id == id);
-//            if (objTemp is null)
-//                throw new Exception($"Object with Id of {id} does exist.");
+            var objTemp = _EntityContext.NotificationsUsers.SingleOrDefault(m => m.Id == id);
+            if (objTemp is null)
+                throw new Exception($"Object with Id of {id} does exist.");
 
-//            return ConvertNotification_ToAccessorContract(objTemp);
-//        }
+            return ConvertNotificationsUser_ToAccessorContract(objTemp);
+        }
 
-//        public Notification CreateNotification(Notification obj)
-//        {
-//            var newObj = ConvertNotification_ToAccessorModel(obj);
+        public NotificationsUser CreateNotificationUser(NotificationsUser obj)
+        {
+            var newObj = ConvertNotificationsUser_ToAccessorModel(obj);
 
-//            _EntityContext.Notifications.Add(newObj);
-//            _EntityContext.SaveChanges();
+            _EntityContext.NotificationsUsers.Add(newObj);
+            _EntityContext.SaveChanges();
 
-//            return ConvertNotification_ToAccessorContract(newObj);
-//        }
+            return ConvertNotificationsUser_ToAccessorContract(newObj);
+        }
 
-//        public NotificationsUser UpdateNotification(Notification obj)
-//        {
-//            if (obj.Id <= 0)
-//                throw new Exception("Cannot update object without an Id.");
-//            var objTemp = _EntityContext.NotificationsUsers.SingleOrDefault(m => m.Id == obj.Id);
-//            if (objTemp is null)
-//                throw new Exception($"Object with Id of {obj.Id} does not exist.");
+        public NotificationsUser UpdateNotificationUser(NotificationsUser obj)
+        {
+            if (obj.Id <= 0)
+                throw new Exception("Cannot update object without an Id.");
+            var objTemp = _EntityContext.NotificationsUsers.SingleOrDefault(m => m.Id == obj.Id);
+            if (objTemp is null)
+                throw new Exception($"Object with Id of {obj.Id} does not exist.");
 
-//            objTemp.Id = obj.Id;
-//            objTemp.Title = obj.Title;
-//            objTemp.Message = obj.Message;
-//            objTemp.StatusId = obj.StatusId;
+            objTemp.Id = obj.Id;
+            objTemp.IsRead = obj.IsRead;
+            objTemp.UserId = obj.UserId;
+            objTemp.StatusId = obj.StatusId;
 
-//            _EntityContext.Notifications.Update(objTemp);
-//            _EntityContext.SaveChanges();
+            _EntityContext.NotificationsUsers.Update(objTemp);
+            _EntityContext.SaveChanges();
 
-//            return ConvertNotificationsUser_ToAccessorContract(objTemp);
-//        }
+            return ConvertNotificationsUser_ToAccessorContract(objTemp);
+        }
 
-//        public NotificationsUser DeleteNotification(NotificationsUser obj, bool hardDelete)
-//        {
-//            if (obj.Id <= 0)
-//                throw new Exception("Cannot delete object without an Id.");
-//            var objTemp = _EntityContext.NotificationsUsers.SingleOrDefault(m => m.Id == obj.Id);
-//            if (objTemp is null)
-//                throw new Exception($"Object with Id of {obj.Id} does not exist.");
+        public NotificationsUser DeleteNotification(NotificationsUser obj, bool hardDelete)
+        {
+            if (obj.Id <= 0)
+                throw new Exception("Cannot delete object without an Id.");
+            var objTemp = _EntityContext.NotificationsUsers.SingleOrDefault(m => m.Id == obj.Id);
+            if (objTemp is null)
+                throw new Exception($"Object with Id of {obj.Id} does not exist.");
 
-//            objTemp.StatusId = 0;
-//            var newUser = (hardDelete == true) ? _EntityContext.NotificationsUsers.Remove(objTemp) : _EntityContext.NotificationsUsers.Update(objTemp);
+            objTemp.StatusId = 0;
+            var newUser = (hardDelete == true) ? _EntityContext.NotificationsUsers.Remove(objTemp) : _EntityContext.NotificationsUsers.Update(objTemp);
 
-//            _EntityContext.SaveChanges();
-//            return ConvertNotificationsUser_ToAccessorContract(objTemp);
-//        }
+            _EntityContext.SaveChanges();
+            return ConvertNotificationsUser_ToAccessorContract(objTemp);
+        }
 
-//        private NotificationsUser ConvertNotificationsUser_ToAccessorContract(Models.NotificationsUser tempitem)
-//        {
-//            var newObj = new NotificationsUser
-//            {
-//                Id = tempitem.Id,
-//                IsRead = tempitem.IsRead,
-//                UserId = tempitem.UserId,
-//                StatusId = tempitem.StatusId
-//            };
+        private NotificationsUser ConvertNotificationsUser_ToAccessorContract(Models.NotificationsUser tempitem)
+        {
+            var newObj = new NotificationsUser
+            {
+                Id = tempitem.Id,
+                IsRead = tempitem.IsRead,
+                UserId = tempitem.UserId,
+                StatusId = tempitem.StatusId
+            };
 
-//            return newObj;
-//        }
+            return newObj;
+        }
 
-//        private Models.NotificationsUser ConvertNotificationsUser_ToAccessorModel(NotificationsUser tempitem)
-//        {
-//            var newObj = new Models.NotificationsUser
-//            {
-//                Id = tempitem.Id,
-//                IsRead = tempitem.IsRead,
-//                UserId = tempitem.UserId,
-//                StatusId = tempitem.StatusId
-//            };
+        private Models.NotificationsUser ConvertNotificationsUser_ToAccessorModel(NotificationsUser tempitem)
+        {
+            var newObj = new Models.NotificationsUser
+            {
+                Id = tempitem.Id,
+                IsRead = tempitem.IsRead,
+                UserId = tempitem.UserId,
+                StatusId = tempitem.StatusId
+            };
 
-//            return newObj;
-//        }
+            return newObj;
+        }
 
-//    }
-//}
+    }
+}
